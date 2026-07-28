c560e04cb2a063a6303e8a6b4d9abf4c987c7e97
Raced <110767054+decarsanchez10@users.noreply.github.com>
2026-07-29 00:04:39 +0800

    Delete download_deps.ps1

--- download_deps.ps1
+++
@@ -1,6 +0,0 @@
-$url = "https://repo1.maven.org/maven2/com/formdev/flatlaf/3.4.1/flatlaf-3.4.1.jar"
-$output = "flatlaf-3.4.1.jar"
-
-Write-Host "Downloading FlatLaf..."
-Invoke-WebRequest -Uri $url -OutFile $output
-Write-Host "Download complete: $output"
