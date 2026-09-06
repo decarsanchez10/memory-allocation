a6e5dad8d5312c54e620f4ea3aee9bedcf28669d
decarsanchez10 <decarsanchez1@gmail.com>
2026-09-07 06:57:34 +0800

    chore: remove compiled class files from source directory

--- backup/MemoryAllocationGUI.java
+++
@@ -1,7 +0,0 @@
-package backup;
-// Replaced old implementation with entry point to new UI
-public class MemoryAllocationGUI {
-    public static void main(String[] args) {
-        MainDashboard.main(args);
-    }
-}
